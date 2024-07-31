757a9168c8d1ec6c441573015c7e642bc3c0c4e0
Mayusha-R <rathodmayusha@gmail.com>
2024-07-31 19:01:16 +0530

    Changed java file

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Java in Docker from Ansible!");
+        System.out.println("Hello Java in Docker from Ansible(Development)!");
     }
 }
